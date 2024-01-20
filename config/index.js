2b4a3f71ea114fe98946a92dfa6dd3df5798c985
yangxijia <111684520+yangxijia@users.noreply.github.com>
2024-01-20 12:39:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx24903f8a66e8c664",
+  appsecret: "e13f53d8b270530f031ef9bea2bb8fa1",
+  template_id: "hEd_fyqjzZ94NtJVyJsB6Gt9FW4QrUHAsBnuqU0EBD4",
+  touser: "oTK8X6Zan_ZgSbCqEXcgeyklPoYk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "濮阳",
+  first_date: "2023-11-06",
 };
